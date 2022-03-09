ed5c9cf36dbca416ed73720c3f633425b5f545fd
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-09 15:58:32 +0300

    added role

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -11,35 +11,37 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_iam_role" "ronesans-apprunner-role" {
-  name               = "ronesans-apprunner-role"
-  assume_role_policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
+
+
+resource "aws_iam_role" "role" {
+   name = "test-role"
+   assume_role_policy = jsonencode({
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Action": "sts:AssumeRole",
+       "Principal": {
+         "Service": [
            "build.apprunner.amazonaws.com",
            "tasks.apprunner.amazonaws.com"
          ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-})
+       },
+       "Effect": "Allow",
+       "Sid": ""
+     }
+   ]
+ } )
 }
-
-resource "aws_iam_role_policy_attachment" "runner_role_policy_attachment" {
-  role       = aws_iam_role.ronesans-apprunner-role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
-}
+resource "aws_iam_role_policy_attachment" "test-attach" {
+   role       = aws_iam_role.role.name
+   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+ }
 
 resource "aws_apprunner_service" "ronesans-apprunner" {
   service_name = "ronesans-apprunner"
   source_configuration {
-    authentication_configuration {
-      access_role_arn = aws_iam_role.ronesans-apprunner-role.arn
+    authentication_configuration{
+      access_role_arn = aws_iam_role.role.arn
     }
     image_repository {
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans-ecr:309"
