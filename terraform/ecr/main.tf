94af90e0c7cbbe03f69c4bc3733c427af88eb973
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-09 20:31:21 +0000

    Updated main.tf

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -8,12 +8,13 @@ terraform {
 }
 
 provider "aws" {
-  alias  = "us_west_2"
-  region = "us-west-2"
+  alias  = "us_east_1"
+  region = "us-east-1"
 }
 
-resource "aws_ecrpublic_repository" "ronesans-public-ecr" {
-  provider = aws.us_west_2
+resource "aws_ecrpublic_repository" "ronesanspublicecr" {
+  provider = aws.us_east_1
 
-  repository_name = "ronesans-public-ecr"
+  repository_name = "ronesanspublicecr"
+
 }
