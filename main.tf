febe634796f66a910271cff21ad698e22208630a
02485517 <nevincansel.efendioglu@kocsistem.com.tr>
2022-03-09 11:36:29 +0300

    provider added

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
 resource "aws_instance" "ronesans-poc" {
     ami           = "ami-0d527b8c289b4af7f" # eu-central-1
     instance_type = "t2.micro"
