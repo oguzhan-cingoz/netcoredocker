775d775e378ce7b5ea6ab87e4991946e4564ada2
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-09 15:19:31 +0300

    added role

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -7,6 +7,31 @@ terraform {
   }
 }
 
+resource "aws_iam_role" "role" {
+   name = "ronesans-apprunner-role"
+   assume_role_policy = {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Action": "sts:AssumeRole",
+       "Principal": {
+         "Service": [
+           "build.apprunner.amazonaws.com",
+           "tasks.apprunner.amazonaws.com"
+         ]
+       },
+       "Effect": "Allow",
+       "Sid": ""
+     }
+   ]
+ }  
+}
+
+resource "aws_iam_role_policy_attachment" "ronesans-policy-attach" {
+   role       = aws_iam_role.role.name
+   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+ }
+
 provider "aws" {
   region = "eu-west-1"
 }
