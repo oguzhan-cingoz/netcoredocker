69f661a75589f535e49632224c73782e6955d7a9
02485517 <nevincansel.efendioglu@kocsistem.com.tr>
2022-03-09 16:44:25 +0300

    role added

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -11,9 +11,36 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_iam_role" "ronesans-ar-role" {
+  name               = "ronesans-ar-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = [ 
+            "build.apprunner.amazonaws.com",
+            "tasks.apprunner.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ronesans-ar-role_policy_attachment" {
+  role       = aws_iam_role.ronesans-ar-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
 resource "aws_apprunner_service" "ronesans-apprunner" {
   service_name = "ronesans-apprunner"
   source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.ronesans-ar-role.arn
+    }
     image_repository {
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans-ecr:309"
       image_repository_type = "ECR"
