e206447fb4e750169f34c5e778a705044e71e395
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-09 15:42:11 +0300

    added role

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_iam_role" "runner_role" {
+resource "aws_iam_role" "ronesans-apprunner-role" {
   name               = "ronesans-apprunner-role"
   assume_role_policy = jsonencode({
     Version   = "2012-10-17"
@@ -28,7 +28,7 @@ resource "aws_iam_role" "runner_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "runner_role_policy_attachment" {
-  role       = aws_iam_role.runner_role.name
+  role       = aws_iam_role.ronesans-apprunner-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
@@ -36,7 +36,7 @@ resource "aws_apprunner_service" "runner_service" {
   service_name = "ronesans-apprunner"
   source_configuration {
     authentication_configuration {
-      access_role_arn = aws_iam_role.runner_role.arn
+      access_role_arn = aws_iam_role.ronesans-apprunner-role.arn
     }
     image_repository {
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans-ecr:309"
