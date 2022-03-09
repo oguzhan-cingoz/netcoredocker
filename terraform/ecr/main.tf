4ba9e8cc68c4f7ff13d034c3dbb6834ea9876940
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-09 14:51:09 +0300

    added provider

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
 resource "aws_ecr_repository" "ronesans-ecr" {
   name                 = "ronesans-ecr"
   image_tag_mutability = "MUTABLE"
