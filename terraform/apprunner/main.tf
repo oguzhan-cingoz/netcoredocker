3876d5fc166474d47b766aafc41d722bcd3fe42f
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-09 15:27:37 +0300

    added role

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -7,55 +7,39 @@ terraform {
   }
 }
 
-resource "aws_iam_role" "role" {
-   name = "ronesans-apprunner-role"
-   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+resource "aws_iam_role" "runner_role" {
+  name               = "ronesans-apprunner-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
         Principal = {
-          Service = [
-           "build.apprunner.amazonaws.com",
-           "tasks.apprunner.amazonaws.com"
-         ]
+          Service = "build.apprunner.amazonaws.com"
         }
-      },
+      }
     ]
   })
 }
 
-
-resource "aws_iam_role_policy_attachment" "ronesans-policy-attach" {
-   role       = aws_iam_role.role.name
-   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
- }
-
-provider "aws" {
-  region = "eu-west-1"
+resource "aws_iam_role_policy_attachment" "runner_role_policy_attachment" {
+  role       = aws_iam_role.runner_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
-resource "aws_apprunner_service" "ronesans-poc" {
-  service_name = "ronesans-poc"
-
+resource "aws_apprunner_service" "runner_service" {
+  service_name = local.service_name
   source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.runner_role.arn
+    }
     image_repository {
-      image_configuration {
-        port = 5001
-        
-      }
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans-ecr:309"
       image_repository_type = "ECR"
+      image_configuration {
+        port = local.service_port
+      }
     }
-    authentication_configuration{
-      access_role_arn = aws_iam_role.role.arn
-    }
-    auto_deployments_enabled = true
-  }
-
-  tags = {
-    Name = "ronesans-poc"
   }
 }
