daf0c426159c442b5ee34a1d8ff6a5f95434374a
02485517 <nevincansel.efendioglu@kocsistem.com.tr>
2022-03-09 16:19:53 +0300

    arn

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -41,7 +41,7 @@ resource "aws_apprunner_service" "ronesans-apprunner" {
   service_name = "ronesans-apprunner"
   source_configuration {
     authentication_configuration{
-      access_role_arn = aws_iam_role.role.arn
+      access_role_arn = aws_iam_role.role.policy_arn
     }
     image_repository {
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans-ecr:309"
