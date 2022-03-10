b841087721c56a32e30d89d9baf8398fc3fab308
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-10 20:05:07 +0000

    Updated main.tf

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -14,14 +14,14 @@ provider "aws" {
 
 
 resource "aws_apprunner_service" "ronesans-app-runner" {
-  service_name = "ronesans-app-runner"
+  service_name = "ronesansapprunner"
   source_configuration {
     auto_deployments_enabled = false
     image_repository {
       image_identifier      = "public.ecr.aws/u3h5m1q5/ronesans-public-ecr:v1"
       image_repository_type = "ECR_PUBLIC"
       image_configuration {
-        port = 5001
+        port = 80
       }
     }
   }
