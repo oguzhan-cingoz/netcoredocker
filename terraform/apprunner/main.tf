ed4ddee1a9ac9dbc7afd72f99c5486d84a269dc7
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-10 14:42:58 +0000

    Updated main.tf

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -11,13 +11,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_iam_role" "ronesansacrrole" {
+  name               = "ronesansacrrole"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          AWS: "arn:aws:iam::545579686143:root"
+          Service = [ 
+            "build.apprunner.amazonaws.com",
+            "tasks.apprunner.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+}
 
+resource "aws_iam_role_policy_attachment" "ronesansacrrole_policy_attachment" {
+  role       = aws_iam_role.ronesansacrrole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerFullAccess"
+}
 
 resource "aws_apprunner_service" "ronesans-app-runner" {
   service_name = "ronesans-app-runner"
   source_configuration {
     authentication_configuration {
-      access_role_arn = "arn:aws:iam::545579686143:role/ronesansacrrole"
+      access_role_arn = aws_iam_role.ronesansacrrole.arn
     }
     image_repository {
       image_identifier      = "public.ecr.aws/u3h5m1q5/ronesans-public-ecr:v1"
