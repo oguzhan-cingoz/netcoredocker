22f75b4eeda85ce435b1c0bd86ab73fdd8a50cec
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-10 14:35:57 +0000

    Updated main.tf

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -17,7 +17,7 @@ resource "aws_apprunner_service" "ronesans-app-runner" {
   service_name = "ronesans-app-runner"
   source_configuration {
     authentication_configuration {
-      access_role_arn = arn:aws:iam::545579686143:role/ronesansacrrole
+      access_role_arn = "arn:aws:iam::545579686143:role/ronesansacrrole"
     }
     image_repository {
       image_identifier      = "public.ecr.aws/u3h5m1q5/ronesans-public-ecr:v1"
