70d8dc1cc42beba034bcb6d04e128c9f7d60a2d9
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-11 01:32:50 +0300

    changed private ecr app runner terraform file

--- terraform/apprunner/privateecr/main.tf
+++ terraform/apprunner/privateecr/main.tf
@@ -1,18 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
 provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_iam_role" "ronesansroles" {
-  name = "ronesansroles"
+resource "aws_iam_role" "ronesansrroles" {
+  name = "ronesansrroles"
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
@@ -28,8 +19,8 @@ resource "aws_iam_role" "ronesansroles" {
 })
 }
 
-resource "aws_iam_role_policy_attachment" "ronesansrolespolicy" {
-  role = aws_iam_role.ronesansroles.id
+resource "aws_iam_role_policy_attachment" "ronesansrrolespolicy" {
+  role = aws_iam_role.ronesansrroles.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
@@ -37,7 +28,7 @@ resource "aws_apprunner_service" "ronesans-app-runnerr" {
   service_name = "ronesans-app-runnerr"
   source_configuration {
     authentication_configuration {
-      access_role_arn = "${aws_iam_role.ronesansroles.arn}"
+      access_role_arn = aws_iam_role.ronesansrroles.arn
     }
     image_repository {
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans_privateecr:21478"
