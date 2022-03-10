821b0543dce45c72b76102a61431d72a27bc4c2e
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-11 01:21:32 +0300

    changed private ecr app runner terraform file

--- terraform/apprunner/privateecr/main.tf
+++ terraform/apprunner/privateecr/main.tf
@@ -11,11 +11,33 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_apprunner_service" "ronesans-app-runner" {
-  service_name = "ronesans-app-runner"
+resource "aws_iam_role" "ronesansrole" {
+  name = "ronesansrole"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "build.apprunner.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "ronesansrolepolicy" {
+  role = aws_iam_role.ronesansrole.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+resource "aws_apprunner_service" "ronesans-app-runnerr" {
+  service_name = "ronesans-app-runnerr"
   source_configuration {
     authentication_configuration {
-      access_role_arn = "arn:aws:iam::545579686143:role/ronesansrole"
+      access_role_arn = "aws_iam_role.ronesansrole.arn"
     }
     image_repository {
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans_privateecr:21478"
