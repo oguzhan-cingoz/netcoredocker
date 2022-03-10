554d9aa1051c6b55c7533e8669c42619e52d6c4e
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-10 19:27:24 +0000

    Updated main.tf

--- terraform/apprunner/main.tf
+++ terraform/apprunner/main.tf
@@ -11,71 +11,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "apprunner-service-role" {
-  description = "This role gives App Runner permission to access ECR"
-  default     = "ronesans"
-}
-
-
-data "aws_iam_policy_document" "apprunner-service-assume-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["build.apprunner.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "apprunner-instance-assume-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type = "Service"
-      identifiers = ["tasks.apprunner.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "apprunner-service-role" {
-  name               = "${var.apprunner-service-role}AppRunnerECRAccessRole"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.apprunner-service-assume-policy.json
-}
-
-
-resource "aws_iam_role_policy_attachment" "apprunner-service-role-attachment" {
-  role       = aws_iam_role.apprunner-service-role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
-}
-
-
-resource "aws_iam_role" "apprunner-instance-role" {
-  name = "${var.apprunner-service-role}AppRunnerInstanceRole"
-  path = "/"
-  assume_role_policy = data.aws_iam_policy_document.apprunner-instance-assume-policy.json
-}
-
-resource "aws_iam_policy" "Apprunner-policy" {
-  name = "Apprunner-getSSM"
-  policy = data.aws_iam_policy_document.apprunner-instance-role-policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "apprunner-instance-role-attachment" {
-  role = aws_iam_role.apprunner-instance-role.name
-  policy_arn = aws_iam_policy.Apprunner-policy.arn
-}
-
 
 
 resource "aws_apprunner_service" "ronesans-app-runner" {
   service_name = "ronesans-app-runner"
   source_configuration {
-    authentication_configuration {
-      access_role_arn = aws_iam_role.apprunner-service-role.name
-    }
+    auto_deployments_enabled = false
     image_repository {
       image_identifier      = "public.ecr.aws/u3h5m1q5/ronesans-public-ecr:v1"
       image_repository_type = "ECR_PUBLIC"
