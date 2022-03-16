c2f8d92ab876e3be6d878a8252a967d119471180
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-16 20:51:30 +0000

    Updated main.tf

--- terraform/apprunner/privateecr/main.tf
+++ terraform/apprunner/privateecr/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_iam_role" "rronesansroles" {
-  name = "rronesansroles"
+resource "aws_iam_role" "ronesansroles" {
+  name = "ronesansroles"
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
@@ -19,17 +19,23 @@ resource "aws_iam_role" "rronesansroles" {
 })
 }
 
-resource "aws_iam_role_policy_attachment" "rronesansrolespolicy" {
-  role = aws_iam_role.rronesansroles.id
+resource "aws_iam_role_policy_attachment" "ronesansrolespolicy" {
+  role = aws_iam_role.ronesansroles.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
+resource "time_sleep" "waitrolecreate" {
+  depends_on = [aws_iam_role.ronesansroles]
+
+  create_duration = "60s"
+}
+
 resource "aws_apprunner_service" "ronesans-app-runner" {
-  depends_on = [aws_iam_role.rronesansroles]
+  depends_on = [time_sleep.waitrolecreate]
   service_name = "ronesans-app-runner"
   source_configuration {
     authentication_configuration {
-      access_role_arn = "${aws_iam_role.rronesansroles.arn}"
+      access_role_arn = "${aws_iam_role.ronesansroles.arn}"
     }
     image_repository {
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans_privateecr:21478"
