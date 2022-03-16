7890b2172ecc9eee3a65b3bb673668d77a5fb657
Kadriye Taylan <kadriye.taylan@kocsistem.com.tr>
2022-03-16 15:34:01 +0000

    Updated main.tf

--- terraform/apprunner/privateecr/main.tf
+++ terraform/apprunner/privateecr/main.tf
@@ -1,42 +1,54 @@
 provider "aws" {
   region = "eu-west-1"
 }
-
-resource "aws_iam_role" "ronesansrroles" {
-  name = "ronesansrroles"
-
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "build.apprunner.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-})
+resource "aws_iam_role" "ronesansapprunnerrole" {
+   name = "ronesansapprunnerrole"
+   assume_role_policy = jsonencode(
+{
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Action": "sts:AssumeRole",
+       "Principal": {
+         "Service": [
+           "build.apprunner.amazonaws.com",
+           "tasks.apprunner.amazonaws.com"
+         ]
+       },
+       "Effect": "Allow",
+       "Sid": ""
+     }
+   ]
+ })
 }
 
-resource "aws_iam_role_policy_attachment" "ronesansrrolespolicy" {
-  role = aws_iam_role.ronesansrroles.id
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+resource "aws_iam_role_policy_attachment" "ronesansapprunnerrole-attach" {
+   role       = aws_iam_role.ronesansapprunnerrole.name
+   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+ }
+
+output "caller_arn" {
+  value = "${aws_iam_role.ronesansapprunnerrole.arn}"
 }
 
+resource "aws_apprunner_service" "ronesans-apprunner-service" {
+  service_name = "ronesans_apprunner"
 
-resource "aws_apprunner_service" "ronesans-app-runnerr" {
-  service_name = "ronesans-app-runnerr"
   source_configuration {
-    authentication_configuration {
-      access_role_arn = "arn:aws:iam::545579686143:role/${aws_iam_role.ronesansrroles.name}"
-    }
     image_repository {
+      image_configuration {
+        port = "80"
+      }
       image_identifier      = "545579686143.dkr.ecr.eu-west-1.amazonaws.com/ronesans_privateecr:21478"
       image_repository_type = "ECR"
-      image_configuration {
-        port = 80
-      }
     }
+    authentication_configuration{
+      access_role_arn = aws_iam_role.ronesansapprunnerrole.arn
+    }
+    auto_deployments_enabled = true
+  }
+
+  tags = {
+    Name = "ronesans_apprunner"
   }
 }
